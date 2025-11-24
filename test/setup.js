e6e0e2c253bd97ffaa4cbcc04679b5bafe8a4b5c
harshitha.d <harshitha.d@contentstack.com>
2025-11-24 12:14:27 +0530

    Refactor test setup by removing nock cleanup logic for improved simplicity

--- test/setup.js
+++ test/setup.js
@@ -1,16 +1,6 @@
 /**
- * Test setup file for proper mock cleanup
- * This ensures nock interceptors are properly cleaned between tests
- * Note: sinon cleanup is handled by fancy-test library
+ * Test setup file
  */
-
-const nock = require('nock');
-
-// Global afterEach to clean up nock interceptors
-afterEach(function() {
-  // Clean up all nock interceptors to prevent leakage between tests
-  nock.cleanAll();
-});
 
 module.exports = {
   // Export any shared test utilities here if needed
