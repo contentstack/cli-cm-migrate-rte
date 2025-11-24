aa328f76c90bb971ce809f0db1a6204d282ae4bb
harshitha.d <harshitha.d@contentstack.com>
2025-11-24 13:46:01 +0530

    Add global configuration for test environment to prevent region errors and stub Command class for consistent API URL usage

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,22 @@
 // test/setup.js
 
+const { Command } = require('@contentstack/cli-command');
+
+// Set default region for tests to avoid "Region not configured" error
+process.env.CSDX_REGION = 'NA';
+
+// Set the default host for Command instances to avoid region configuration errors
+const testApiUrl = 'https://api.contentstack.io';
+
+// Stub the Command class's cmaHost getter globally for all tests
+// This prevents "Region not configured" errors during test execution
+Object.defineProperty(Command.prototype, 'cmaHost', {
+  get: function() {
+    return testApiUrl;
+  },
+  configurable: true
+});
+
 // Make unhandled promise rejections fail tests loudly
 process.on("unhandledRejection", (reason) => {
   // eslint-disable-next-line no-console
